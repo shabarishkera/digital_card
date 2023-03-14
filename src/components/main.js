b9ac1f67e5a0ac72093ea7c5aa8ab365080fc07d
shabarish <shabarishkera@gmail.com>
2023-03-14 15:40:42 +0530

    initial commit

--- src/components/main.js
+++ src/components/main.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <div>
-      
+    <div className='about'>
+      <h3>About</h3>
+      <p className='about-desc'> Senior front-end developer with 7+ years of experience designing and building responsive web design and mobile apps in the financial industry. Proficient with CSS and JS Frameworks, with extensive knowledge of UX and user psychology. Notable achievements include boosting the conversion rate of an existing website by 80% with improved code and design.</p>
     </div>
   )
 }
